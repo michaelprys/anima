730fb8f260fb2ae2f17912a102a006a271caf676
musha09cz@gmail.com <musha09cz@gmail.com>
2026-03-21 16:55:30 +0100

    feat: add form validation

--- src/components/notes/NoteForm.vue
+++ src/components/notes/NoteForm.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { useTemplateRef, reactive } from 'vue';
+import { useTemplateRef, reactive, ref } from 'vue';
 import { useStoreNotes } from '@/stores/notes.store.js';
 
 const storeNotes = useStoreNotes();
@@ -9,12 +9,18 @@ const newNote = reactive({
     thought: '',
 });
 
+const attempted = ref(true);
+
 const thoughtRef = useTemplateRef('thoughtRef');
 
 const handleAddNote = () => {
-    if (!newNote.title && !newNote.thought) return;
+    if (!newNote.title || !newNote.thought) {
+        attempted.value = false;
 
-    storeNotes.addNote(newNote);
+        return;
+    }
+
+    storeNotes.addNote({ ...newNote });
 
     Object.assign(newNote, {
         title: '',
@@ -24,28 +30,44 @@ const handleAddNote = () => {
     thoughtRef.value.focus();
 };
 </script>
+
 <template>
     <div class="max-w-4xl mx-auto mb-24">
         <div class="relative group">
             <div
-                class="absolute -inset-1 bg-gradient-to-r from-cyan-500/15 to-blue-500/15 blur-2xl opacity-0 group-focus-within:opacity-100 transition-opacity duration-1000"></div>
+                class="absolute -inset-1 bg-linear-to-r from-cyan-500/15 to-blue-500/15 blur-2xl opacity-0 group-focus-within:opacity-100 transition-opacity duration-1000"></div>
 
-            <div
+            <form
+                @submit.prevent="handleAddNote"
                 class="relative bg-slate-900/40 backdrop-blur-3xl rounded-sm p-6 md:p-8 transition-all duration-500 border border-white/10 focus-within:border-cyan-500/30">
                 <input
                     v-model="newNote.title"
                     maxLength="40"
                     type="text"
-                    placeholder="Title"
-                    class="w-full bg-transparent border-none text-2xl font-light tracking-tight text-white placeholder-slate-500 focus:outline-none focus:ring-0 mb-4" />
+                    :placeholder="attempted || newNote.title ? '> TITLE' : 'REQUIRED _'"
+                    @input="attempted = true"
+                    :class="[
+                        'w-full bg-transparent border-none font-mono text-lg font-black tracking-[0.5em] focus:outline-none focus:ring-0 mb-2 uppercase transition-all duration-500',
+                        attempted || newNote.title
+                            ? 'text-cyan-400 placeholder-cyan-900/50'
+                            : 'text-rose-500 placeholder-rose-800 animate-[pulse_1.5s_infinite]',
+                    ]" />
+                <div class="h-px w-12 bg-white/5 mb-6"></div>
 
                 <textarea
                     v-model="newNote.thought"
                     maxLength="1000"
                     ref="thoughtRef"
-                    placeholder="Vent your thoughts here..."
+                    :placeholder="attempted || newNote.thought ? 'JUST TALK...' : 'REQUIRED _'"
                     rows="5"
-                    class="w-full bg-transparent border-none text-lg text-slate-300 placeholder-slate-500 focus:outline-none focus:ring-0 resize-none leading-relaxed mb-6"></textarea>
+                    @input="attempted = true"
+                    :class="[
+                        'w-full bg-transparent border-none font-mono text-sm tracking-[0.3em] focus:outline-none focus:ring-0 mb-6 uppercase transition-colors resize-none leading-relaxed',
+                        attempted || newNote.thought
+                            ? 'text-slate-300 placeholder-slate-600'
+                            : 'text-rose-500 placeholder-rose-800  animate-[pulse_1.5s_infinite]',
+                    ]"></textarea>
+
                 <div
                     class="flex flex-col sm:flex-row gap-6 items-start sm:items-center justify-between pt-6 border-t border-white/10">
                     <div class="flex flex-wrap gap-x-4 gap-y-2">
@@ -60,13 +82,12 @@ const handleAddNote = () => {
                     </div>
 
                     <button
-                        @click="handleAddNote"
-                        :disabled="!newNote.title && !newNote.thought"
-                        class="w-full sm:w-auto px-6 py-2 text-[10px] font-black uppercase tracking-[0.3em] border border-cyan-500/40 text-cyan-400 hover:bg-cyan-500 hover:text-slate-950 hover:border-cyan-500 transition-all duration-500 rounded-[2px] active:scale-95 shadow-lg shadow-cyan-500/5 disabled:opacity-40 disabled:cursor-not-allowed disabled:hover:bg-transparent disabled:hover:text-cyan-400 disabled:hover:border-cyan-500/40 disabled:active:scale-100">
+                        type="submit"
+                        class="w-full sm:w-auto px-8 py-2 text-[10px] font-black uppercase tracking-[0.3em] border border-cyan-500/40 text-cyan-400 hover:bg-cyan-500 hover:text-slate-950 hover:border-cyan-500 transition-all duration-500 rounded-xs active:scale-95 shadow-lg shadow-cyan-500/5">
                         Commit
                     </button>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 </template>
